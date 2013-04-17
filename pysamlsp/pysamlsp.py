a2d7db3ef40fe46eda4714ffb64a1e18a2f28434
Rob Martin <rob@version2beta.com>
2013-04-17 23:41:41 +0000

    Added xmlsec signing

--- pysamlsp/pysamlsp.py
+++ pysamlsp/pysamlsp.py
@@ -1,17 +1,43 @@
+import os
 import uuid
+from sh import xmlsec1
 from datetime import datetime
 from lxml import etree
 from lxml.builder import ElementMaker
+
+XML_SIGNATURE_FRAGMENT = """
+<ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+  <ds:SignedInfo> 
+    <ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/> 
+    <ds:SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/>
+    <ds:Reference>
+      <ds:Transforms> 
+        <ds:Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/> 
+        <ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/> 
+      </ds:Transforms> 
+      <ds:DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/> 
+      <ds:DigestValue /> 
+    </ds:Reference> 
+  </ds:SignedInfo> 
+  <ds:SignatureValue /> 
+  <ds:KeyInfo> 
+    <ds:X509Data /> 
+  </ds:KeyInfo> 
+</ds:Signature>
+"""
 
 def iso_no_microseconds(dt):
   return(datetime.isoformat(dt.replace(microsecond = 0)))
 
 class Pysamlsp(object):
   def __init__(self, config = {}):
+    self.ID = uuid.uuid4().hex
     self.assertion_consumer_service_url = config.get(
       'assertion_consumer_service_url'
     ) or ''
     self.issuer = config.get('issuer') or ''
+    self.private_key = config.get('private_key') or ''
+    self.public_key = config.get('public_key') or ''
   def samlp_maker(self):
     return ElementMaker(
       namespace='urn:oasis:names:tc:SAML:2.0:protocol',
@@ -22,15 +48,19 @@ class Pysamlsp(object):
       namespace='urn:oasis:names:tc:SAML:2.0:assertion',
       nsmap=dict(saml='urn:oasis:names:tc:SAML:2.0:assertion'),
     )
-  def authnrequest_maker(self):
+  def authnrequest(self):
     authn_request = self.samlp_maker().AuthnRequest(
       ProtocolBinding = 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
       Version = '2.0',
       IssueInstant = iso_no_microseconds(datetime.utcnow()),
-      ID = uuid.uuid4().hex,
+      ID = self.ID,
       AssertionConsumerServiceURL = self.assertion_consumer_service_url
     )
     authn_request.append(self.saml_maker().Issuer(self.issuer))
+    authn_request.append(self.samlp_maker().NameIDPolicy(
+      allowcreate='true',
+      format='urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
+    ))
     requested_authn_context = self.samlp_maker().RequestedAuthnContext(
       Comparison = "exact"
     )
@@ -38,5 +68,23 @@ class Pysamlsp(object):
       self.saml_maker().AuthnContextClassRef('urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport')
     requested_authn_context.append(requested_authn_context_class_ref)
     authn_request.append(requested_authn_context)
-    return etree.tostring(authn_request)
+    return authn_request
+  def authnrequest_as_string(self):
+    return etree.tostring(self.authnrequest())
+  def authnrequest_to_sign(self):
+    authnrequest_to_sign = self.authnrequest()
+    authnrequest_to_sign.append(
+      etree.fromstring(XML_SIGNATURE_FRAGMENT))
+    return etree.tostring(authnrequest_to_sign)
+  def authnrequest_signed(self):
+    tempfile = '/tmp/' + self.ID
+    with open(tempfile, 'w') as fh:
+      fh.write(self.authnrequest_to_sign())
+    signed = xmlsec1(
+        '--sign',
+        '--privkey-pem', self.private_key,
+        '--pubkey-pem', self.public_key,
+        tempfile)
+    #os.remove(tempfile)
+    return signed.stdout
 
--- pysamlsp/test_pysamlsp.py
+++ pysamlsp/test_pysamlsp.py
@@ -16,52 +16,86 @@ class TestUtilityFunctions(unittest.TestCase):
 class TestPysamlspAuthnRequestRoot(unittest.TestCase):
   def test_the_root_element_namespace(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar.tag) == '{urn:oasis:names:tc:SAML:2.0:protocol}AuthnRequest'
   def test_the_root_ProtocolBinding_attribute(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar.get('ProtocolBinding')) == 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
   def test_the_root_Version_attribute(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar.get('Version')) == '2.0'
   def test_the_root_IssueInstant_attribute(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar.get('IssueInstant')) == iso_no_microseconds(datetime.utcnow())
   def test_the_root_ID_attribute(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(all(c in string.hexdigits for c in ar.get('ID')))
     expect(len(ar.get('ID'))) == 32
   def test_the_root_AssertionConsumerServiceURL_attribute(self):
     sp = Pysamlsp({'assertion_consumer_service_url': 'http://localhost'})
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar.get('AssertionConsumerServiceURL')) == 'http://localhost'
 
 class TestPysamlspIssuerElement(unittest.TestCase):
   def test_the_element_namespace(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar[0].tag) == '{urn:oasis:names:tc:SAML:2.0:assertion}Issuer'
   def test_the_element_text(self):
     sp = Pysamlsp({'issuer': 'http://localhost/SAML'})
-    ar = etree.fromstring(sp.authnrequest_maker())
+    ar = etree.fromstring(sp.authnrequest_as_string())
     expect(ar[0].text) == 'http://localhost/SAML'
+
+class TestPysamlspNameIDPolicyElement(unittest.TestCase):
+  def test_the_element_namespace(self):
+    sp = Pysamlsp()
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[1].tag) == \
+      '{urn:oasis:names:tc:SAML:2.0:protocol}NameIDPolicy'
+  def test_the_root_allowcreate_attribute(self):
+    sp = Pysamlsp()
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[1].get('allowcreate')) == 'true'
+  def test_the_root_format_attribute(self):
+    sp = Pysamlsp()
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[1].get('format')) == \
+      'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
 
 class TestPysamlspRequestedAuthnContextElement(unittest.TestCase):
   def test_the_element_namespace(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
-    expect(ar[1].tag) == \
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[2].tag) == \
       '{urn:oasis:names:tc:SAML:2.0:protocol}RequestedAuthnContext'
   def test_the_element_Comparison_attribute(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
-    expect(ar[1].get('Comparison')) == 'exact'
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[2].get('Comparison')) == 'exact'
   def test_the_child_element_text(self):
     sp = Pysamlsp()
-    ar = etree.fromstring(sp.authnrequest_maker())
-    expect(ar[1][0].text) == \
+    ar = etree.fromstring(sp.authnrequest_as_string())
+    expect(ar[2][0].text) == \
       'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
+
+class TestAuthnRequestToSign(unittest.TestCase):
+  def test_signature_element(self):
+    sp = Pysamlsp()
+    ar = etree.fromstring(sp.authnrequest_to_sign())
+    expect(
+      ar.xpath( "ds:Signature",
+        namespaces = {'ds': 'http://www.w3.org/2000/09/xmldsig#'})[0].tag
+    ) == '{http://www.w3.org/2000/09/xmldsig#}Signature'
+
+class TestAuthnSignedRequest(unittest.TestCase):
+  def test_signed_element(self):
+    sp = Pysamlsp({'public_key': 'support/saml.pub',
+      'private_key': 'support/saml_key.pem'})
+    ar = etree.fromstring(sp.authnrequest_signed())
+    expect(len(ar.xpath( "//ds:SignatureValue",
+        namespaces = {'ds': 'http://www.w3.org/2000/09/xmldsig#'}))) > 0
+
