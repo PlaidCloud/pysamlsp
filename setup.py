56495de68eeecf53b7f751dcf3cea986dd1e4604
Rob Martin <rob@version2beta.com>
2013-05-08 17:46:24 +0000

    test for xmlsec1

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from setuptools import setup
-from pysamlsp import __version__
+try:
+  from sh import xmlsec1
+except:
+  print "IMPORTANT: the xmlsec1 system package is required by this library. Please remember to also install it through whatever means are appropriate on your operating system."
+
 
 setup(
   name = 'pysamlsp',
-  version = __version__,
+  version = '0.1.4',
   author = "Rob Martin @version2beta",
   author_email = "rob@version2beta.com",
   description = "A service provider implementation for SAML2.0.",
   long_description = open('README.rst').read(),
+  url='http://pypi.python.org/pypi/pysamlsp',
   license = "LICENSE.txt",
-  keywords = "saml service provider",
+  keywords = "SAML2 SSO single sign on service provider only, for use with a remote IdP.",
   packages = ['pysamlsp'],
   install_requires = [
-    'lxml'
+    'lxml',
+    'sh'
   ],
-  test_requires = [
+  tests_require = [
     'nose',
     'expecter',
     'dingus'
   ],
-  package_data = {
-    '': ['*.dist'],
-  }
+  include_package_data = True
 )
 
